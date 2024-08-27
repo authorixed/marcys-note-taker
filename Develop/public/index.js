a6cc94accd633ed30176c7db3fa3e5567b916e9f
Josh Levy <joshlevycatches@gmail.com>
2024-08-27 18:50:08 -0400

    ..........

--- Develop/public/index.js
+++ Develop/public/index.js
@@ -10,23 +10,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use(express.static(path.join(__dirname, 'develop/public')));
 
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'develop/public/notes.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'develop/public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'develop/public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
-
 
 app.get("/api/notes", (req, res) => {
   fs.readFile("./develop/db/db.json", "utf8", (err, data) => {
